e325a06b101448a60fdcf1b50fed9b2b089b2126
kflint2 <kflint2@gmail.com>
2015-06-24 18:59:44 -0600

    Url fields now disappear when one of the other 3 have been text in them. This stops the user from adding multiple. Got youtube url to work by using trustresourceurl

--- public/views/posts/posts.js
+++ public/views/posts/posts.js
@@ -10,33 +10,36 @@ app.config(function ($routeProvider) {
 });
 
 app.factory("Post", function () {
-    
-        var post = function () {
-            this.url = "";
-            this.title = "";
-            this.description = "";
-            this.votes = 5;
-            this.date = Date.now();
-            this.comments = [{
-                text: "",
-                date: Date.now()
+
+    var post = function () {
+        this.url = "";
+        this.imageUrl = "";
+        this.videoUrl = "";
+        this.title = "";
+        this.description = "";
+        this.votes = 5;
+        this.date = Date.now();
+        this.comments = [{
+            text: "",
+            date: Date.now()
        }];
-        };
-    
+    };
+
     return post;
-    
+
 });
 
 app.service('dataService', function () {
 
     //posts is test data and will go away when the server is in place
     var posts = [];
-
+    
     this.createPost = function (post) {
         posts.push(post); //Will actually $http.post to server
     };
 
     this.getPosts = function () {
+      
         return posts; //Will actually be $http.get
     };
 
@@ -50,14 +53,14 @@ app.service('dataService', function () {
 
 });
 
-app.controller('postCtrl', function ($scope, Post, dataService) {
+app.controller('postCtrl', function ($scope, Post, dataService, $sce) {
     $scope.posts = dataService.getPosts();
     $scope.post = new Post();
     $scope.commentStr = "";
 
     $scope.createPost = function () {
         //write validation code
-        
+        console.log("CLICKED");
         dataService.createPost($scope.post);
         $scope.post = {};
         $scope.posts = dataService.getPosts();
@@ -66,6 +69,15 @@ app.controller('postCtrl', function ($scope, Post, dataService) {
     $scope.addComment = function (post) {
         dataService.addComment($scope.commentStr, post);
     };
+    
+    $scope.getAuthorizedUrl = function(url) {
+        console.log("TEST", url);
+        return $sce.trustAsResourceUrl(url);
+    };
+    //can pull this out
+//    $scope.trustURL = function (url){
+//        $sce.trustAsResourceUrl("https://docs.angularjs.org");
+//    };
 });
 
 // ng-show="posts.count > 0"
